0908b751c500bdd44dfce619f2e9a6d826a880a5
tomabulash <19751968+tomabulash@users.noreply.github.com>
2023-05-26 17:09:26 +0300

    works!

--- scripts.js
+++ scripts.js
@@ -1,6 +1,12 @@
+const debug = document.getElementById('debug');
+const debugClick = document.getElementById('debug-click');
+const debugDisplay = document.getElementById('debug-display');
 const buttonContainer = document.getElementById('button-container');
-const calculation = document.getElementById('calculation');
 const resultDisplay = document.getElementById('result');
+const calcSpanOne = document.getElementById('calc-one');
+const calcSpanTwo = document.getElementById('calc-two');
+const calcSpanThree = document.getElementById('calc-three');
+
 
 const id = ['devide', 'plusMinus', 'DEL', 'clear', 'multiply', 
             'nine', 'eight', 'seven', 'deduct', 'six', 'five', 'four', 'add', 
@@ -14,6 +20,33 @@ const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const firstNum = [];
 let operator = '';
 const secondNum = [];
+
+let debugList = {
+    click: 0,
+    display: 0,
+}
+
+const debugListProxy = new Proxy(debugList, {
+    set: function (target, key, value) {
+        if(key == 'click') {
+            debugClick.textContent = (`${key}: ${value}...` );
+        } else {
+            debugDisplay.textContent = (` ${key}: ${value}`);
+        }
+        target[key] = value;
+        return true;
+    }
+  });
+
+function debugToggle(e) {
+    if(debug.style.display === 'flex') {
+        debug.style.display = 'none';
+        e.target.textContent = 'debug: off';
+    } else {
+        debug.style.display = 'flex';
+        e.target.textContent = 'debug: on';
+    }
+}
 
 function makeButtons() {
     for(let i=0; i<19; i++) {
@@ -69,26 +102,62 @@ let exist = {
 }
 
 function display(arr, opr, arr2) {
-    let str = arr.join('');
+    
+    let str = '';
     let str2 = '';
 
+    if(arr) str = arr.join('');
     if(arr2) str2 = arr2.join('');
 
     if(arr && !opr && !arr2) {
         //display arr
-        return (str.length >= 19) ? str.slice(-19) : str;
+        debugListProxy.display = 1;
+        calcSpanOne.textContent = (str.length >= 19) ? str.slice(-19) : str;
+        calcSpanTwo.textContent = '';
+        calcSpanThree.textContent = '';
+        return 0;
 
     } else if(arr && opr && !arr2) {
         //display arr and opr
-        return (str.length >= 18) ? str.slice(-19) + opr : str + opr;
+        debugListProxy.display = 2;
+        calcSpanOne.textContent = (str.length >= 18) ? str.slice(-19) : str;
+        calcSpanTwo.textContent = opr;
+        calcSpanThree.textContent = '';
+        return 0;
 
     } else if(arr && opr && arr2) {
         //display arr and opr and arr2
-        if(str.length + 1 + str2.length >= 19) {
-            return str.concat(opr, str2).slice(-19);
-        } else {
-            return str + opr + str2;
+        debugListProxy.display = 3;
+        if(str2.length >= 19) {
+            //before 123+7890[12345678901234567890], after [12345678901234567890]
+            debugListProxy.display = 3.1;
+            calcSpanOne.textContent = '';
+            calcSpanTwo.textContent = '';
+            calcSpanThree.textContent = str2.slice(-19);
+            return 0;
+        } else if(str2.length >= 18) {
+            //before 123[+1234567890123456789], after [+1234567890123456789]
+            debugListProxy.display = 3.2;
+            calcSpanOne.textContent = ''
+            calcSpanTwo.textContent = opr;
+            calcSpanThree.textContent = str2.slice(-18);
+            return 0;
+        }else {
+            //before 123[456+7890123456789012]
+            debugListProxy.display = 3.3;
+            calcSpanOne.textContent = str.slice(-(18-str2.length));
+            calcSpanTwo.textContent = opr;
+            calcSpanThree.textContent = str2;
+            return 0;
         }
+
+    } else if(!arr && !opr && !arr2) {
+        //display empty
+        debugListProxy.display = 4;
+        calcSpanOne.textContent = '';
+        calcSpanTwo.textContent = '';
+        calcSpanThree.textContent = '';
+        return;
     }
 }
 
@@ -111,52 +180,60 @@ function delClick() {
     exist.checkOnlyOneChar();
 
     if(!exist.firstNum && !exist.operator && !exist.secondNum) {
+        debugListProxy.click = 1.1;
         //before[], after[]    
-        return 0;
+        return 1.1;
 
     } else if(exist.firstOnlyZeroDot && !exist.operator) {
         //before[0.], after[] 
+        debugListProxy.click = 1.2;
         firstNum.pop();
         firstNum.pop();
-        calculation.textContent = '';
-        return 0;
+        display();
+        return 1.2;
 
     } else if(exist.firstOnlyOneChar && !exist.operator && !exist.secondNum) {
         //before[1], after[]
+        debugListProxy.click = 1.3;
         firstNum.pop();
-        calculation.textContent = '';
-        return 0;
+        display();
+        return 1.3;
 
     } else if(exist.firstNum && !exist.operator && !exist.secondNum) {
         //before[123], after[12]
+        debugListProxy.click = 1.4;
         firstNum.pop();
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 1.4;
 
     } else if(exist.firstNum && exist.operator && !exist.secondNum) {
         //before[123+], after[123]
+        debugListProxy.click = 1.5;
         operator = '';
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 1.5;
 
     } else if(exist.secondOnlyZeroDot) {
-        //before[123+0.], after[123+] 
+        //before[123+0.], after[123+]
+        debugListProxy.click = 1.6; 
         secondNum.pop();
         secondNum.pop();
-        calculation.textContent = display(firstNum, operator);
-        return 0;
+        display(firstNum, operator);
+        return 1.6;
 
     } else if(exist.secondOnlyOneChar) {
         //before[123+4], after[123+]
+        debugListProxy.click = 1.7;
         secondNum.pop();
-        calculation.textContent = display(firstNum, operator);
-        return 0;
+        display(firstNum, operator);
+        return 1.7;
 
     } else if(exist.firstNum && exist.operator && exist.secondNum) {
         //before[123+456], after[123+45]
+        debugListProxy.click = 1.8;
         secondNum.pop();
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 1.8;
 
     }
 }
@@ -166,27 +243,31 @@ function numClick(buttonChar) {
 
     if(!exist.firstNum && !exist.operator && !exist.secondNum) {
         //before[], after[1]
+        debugListProxy.click = 2.1;
         firstNum.push(buttonChar);
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 2.1;
 
     } else if(exist.firstNum && !exist.operator && !exist.secondNum) {
         //before[123], after[1234]
+        debugListProxy.click = 2.2;
         firstNum.push(buttonChar);
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 2.2;
 
     } else if(exist.firstNum && exist.operator && !exist.secondNum) {
         //before[123+], after[123+4]
+        debugListProxy.click = 2.3;
         secondNum.push(buttonChar);
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 2.3;
 
     } else if(exist.firstNum && exist.operator && exist.secondNum) {
         //before[123+456], after[123+4567]
+        debugListProxy.click = 2.4;
         secondNum.push(buttonChar);
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 2.4;
     
     } 
 }
@@ -196,59 +277,67 @@ function plusMinusClick() {
 
     if(!exist.firstNum && !exist.operator && !exist.secondNum) {
         //before[], after[-]
+        debugListProxy.click = 3.1;
         firstNum.unshift('-');
         exist.minus = true;
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 3.1;
 
     } else if(!exist.firstNum && exist.minus && !exist.operator && !exist.secondNum ) {
         //before[-], after[]
+        debugListProxy.click = 3.2;
         firstNum.shift('');
         exist.minus = false;
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 3.2;
 
     } else if(exist.firstNum && !exist.minus && !exist.operator && !exist.secondNum) {
         //before[123], after[-123]
+        debugListProxy.click = 3.3;
         firstNum.unshift('-');
         exist.minus = true;
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 3.3;
 
     } else if(exist.firstNum && exist.minus && !exist.operator && !exist.secondNum ) {
         //before[-123], after[123]
+        debugListProxy.click = 3.4;
         firstNum.shift('');
         exist.minus = false;
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 3.4;
 
     } else if(exist.firstNum && exist.operator && !exist.secondNum) {
-        //before[123+], after[123+-] adds '-' to secondNum
+        //before[123+], after[123+-] 
+        debugListProxy.click = 3.5;
         secondNum.unshift('-');
         exist.minus = true;
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 3.5;
 
     } else if(exist.firstNum && exist.minus && exist.operator && !exist.secondNum) {
-        //before[123+-], after[123+] 
+        //before[123+-], after[123+]
+        debugListProxy.click = 3.6;
         secondNum.shift('');
         exist.minus = false;
-        calculation.textContent = display(firstNum, operator);
-        return 0;
+        display(firstNum, operator);
+        return 3.6;
 
     } else if(exist.firstNum && exist.operator && !exist.minus && exist.secondNum) {
         //before[123+456], after[123+-456]
+        debugListProxy.click = 3.7;
         secondNum.unshift('-');
         exist.minus = true;
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 3.7;
     
     } else if(exist.firstNum && exist.operator && exist.minus && exist.secondNum) {
         //before[123+-456], after[123+456]
+        debugListProxy.click = 3.8;
         secondNum.shift('');
         exist.minus = false;
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 3.8;
 
     }
 }
@@ -259,37 +348,43 @@ function dotClick(buttonChar) {
 
     if(!exist.firstNum && !exist.operator && !exist.secondNum) {
         //before[], after[0.]
+        debugListProxy.click = 4.1;
         firstNum.push('0');
         firstNum.push('.');
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 4.1;
 
     } else if(exist.firstNum && exist.firstHasDot && !exist.operator) {
         //before[0.], after[0.] //before[1.23], after[1.23]
-        return 0;
+        debugListProxy.click = 4.2;
+        return 4.2;
 
     } else if(exist.firstNum && !exist.firstHasDot && !exist.operator) { 
         //before[123], after[123.] 
+        debugListProxy.click = 4.3;
         firstNum.push(buttonChar);
-        calculation.textContent = display(firstNum);
-        return 0;
+        display(firstNum);
+        return 4.3;
 
     } else if(exist.firstNum && exist.operator && !exist.secondNum ) {
         //before[0.123+], after[0.123+0.] //before[123+], after[123+0.]
+        debugListProxy.click = 4.4;
         secondNum.push('0');
         secondNum.push('.');
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 4.4;
 
     } else if(exist.firstNum && exist.operator && exist.secondNum && exist.secondHasDot ) {
         //before[0.123+0.], after[0.123+0.] //before[0.123+12.3], after[0.123+12.3] 
-        return 0;
+        debugListProxy.click = 4.5;
+        return 4.5;
 
     } else if(exist.firstNum && exist.operator && exist.secondNum && !exist.secondHasDot ) { //here
         //before[0.123+456], after[0.123+456.] 
+        debugListProxy.click = 4.6;
         secondNum.push(buttonChar);
-        calculation.textContent = display(firstNum, operator, secondNum);
-        return 0;
+        display(firstNum, operator, secondNum);
+        return 4.6;
 
     } 
 }
@@ -302,28 +397,34 @@ function operatorClick(buttonChar) {
 
     if(!exist.firstNum) {
         //before[], after[]
-        return 0;
+        debugListProxy.click = 5.1;
+        return 5.1;
 
     } else if(exist.firstDotIsLast && !exist.operator) {
         //before[0.], after[0.] //before[123.], after[123.]
-        return 0;
+        debugListProxy.click = 5.2;
+        return 5.2;
 
     } else if(exist.firstNum && !exist.operator) { 
-        //before[123], after[123+] 
+        //before[123], after[123+]
+        debugListProxy.click = 5.3;
         operator = buttonChar;
-        calculation.textContent = display(firstNum, operator);
-        return 0;
+        display(firstNum, operator);
+        return 5.3;
 
     } else if(exist.firstNum && exist.operator && !exist.secondNum ) {
         //before[123+], after[123+]
-        return 0;
+        debugListProxy.click = 5.4;
+        return 5.4;
 
     } else if(exist.firstNum && exist.operator && exist.secondDotIsLast) {
         //before[123+0.], after[123+0.]
-        return 0;
+        debugListProxy.click = 5.5;
+        return 5.5;
 
     } else if(exist.firstNum && exist.operator && exist.secondNum) { 
         //before[123+456], after[result+][result] 
+        debugListProxy.click = 5.6;
         resultDisplay.textContent = calculate();
         firstNum.splice(0, firstNum.length);
         for(const obj of Array.from(resultDisplay.textContent)) {
@@ -331,12 +432,13 @@ function operatorClick(buttonChar) {
         }
         operator = buttonChar;
         secondNum.splice(0, secondNum.length);
-        calculation.textContent = display(firstNum, operator);
-        return 0;
+        display(firstNum, operator);
+        return 5.6;
     
     } else if(exist.firstNum && exist.operator && exist.result && !exist.secondNum) { 
         //before[result+][result], after[result+][result] 
-        return 0;
+        debugListProxy.click = 5.7;
+        return 5.7;
 
     } 
 }
@@ -349,26 +451,32 @@ function equalClick() {
 
     if(!exist.firstNum) {
         //before[], after[]
-        return 0;
+        debugListProxy.click = 6.1;
+        return 6.1;
 
     } else if(exist.firstDotIsLast && !exist.operator) {
         //before[0.], after[0.] //before[123.], after[123.]
-        return 0;
+        debugListProxy.click = 6.2;
+        return 6.2;
 
     } else if(exist.firstNum && !exist.operator) { 
         //before[123], after[123]
-        return 0;
+        debugListProxy.click = 6.3;
+        return 6.3;
 
     } else if(exist.firstNum && exist.operator && !exist.secondNum ) {
         //before[123+], after[123+]
-        return 0;
+        debugListProxy.click = 6.4;
+        return 6.4;
 
     } else if(exist.firstNum && exist.operator && exist.secondDotIsLast) {
         //before[123+0.], after[123+0.]
-        return 0;
+        debugListProxy.click = 6.5;
+        return 6.5;
 
     } else if(exist.firstNum && exist.operator && exist.secondNum) { 
-        //before[123+456], after[123+456][result] 
+        //before[123+456], after[123+456][result]
+        debugListProxy.click = 6.6; 
         resultDisplay.textContent = calculate();
         firstNum.splice(0, firstNum.length);
         for(const obj of Array.from(resultDisplay.textContent)) {
@@ -376,35 +484,39 @@ function equalClick() {
         }
         operator = '';
         secondNum.splice(0, secondNum.length);
-        return 0;
+        return 6.6;
     
     } else if(exist.firstNum && exist.operator && exist.result && !exist.secondNum) { 
-        //before[result+][result], after[result+][result] 
-        return 0;
+        //before[result+][result], after[result+][result]
+        debugListProxy.click = 6.7;
+        return 6.7;
 
     } 
 }
 
 function clearClick() {
+    debugListProxy.click = 7.1;
     firstNum.splice(0, firstNum.length);
     operator = '';
     secondNum.splice(0, secondNum.length);
-    calculation.textContent = '';
+    calcSpanOne.textContent = '';
+    calcSpanTwo.textContent = '';
+    calcSpanThree.textContent = '';
     resultDisplay.textContent = '';
     exist.reset();
-    return 0;
+    return 7.1;
 }
 
 function clickButton(e) {   
     const buttonChar = e.target.textContent;
 
-    if(operators.includes(buttonChar)) operatorClick(buttonChar);
-    if(numbers.includes(buttonChar)) numClick(buttonChar);
-    if(buttonChar == 'DEL') delClick();
-    if(buttonChar == 'C') clearClick();
-    if(buttonChar == '+/-') plusMinusClick();
-    if(buttonChar == '=') equalClick();
-    if(buttonChar == '.') dotClick(buttonChar);    
+    if(operators.includes(buttonChar)) return operatorClick(buttonChar);
+    if(numbers.includes(buttonChar)) return numClick(buttonChar);
+    if(buttonChar == 'DEL') return delClick();
+    if(buttonChar == 'C') return clearClick();
+    if(buttonChar == '+/-') return plusMinusClick();
+    if(buttonChar == '=') return equalClick();
+    if(buttonChar == '.') return dotClick(buttonChar);
 }
 
 //interface effects :
@@ -427,6 +539,7 @@ function up(e) {
     if(e.target.id == 'button-container') return 0;
     e.target.style.filter = 'brightness(0.8)';
     clickButton(e);
+    
 }
 
 makeButtons();
@@ -434,4 +547,5 @@ makeButtons();
 buttonContainer.addEventListener('mouseover', hoverOn);
 buttonContainer.addEventListener('mouseleave', hoverOff);
 buttonContainer.addEventListener('mousedown', down);
-buttonContainer.addEventListener('mouseup', up)
+buttonContainer.addEventListener('mouseup', up);
+document.getElementById('debug-button').addEventListener('click', debugToggle);
